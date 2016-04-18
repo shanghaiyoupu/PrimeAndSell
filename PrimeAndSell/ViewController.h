99fe5824025bf2f1b67bc36ab25dde5ee5c8c5ca
上海柚铺 <shanghaiyoupu@shanghaiyoupudemacbook-pro.local>
2016-04-18 10:18:40 +0800

    Initial Commit

---
+++ PrimeAndSell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrimeAndSell
+//
+//  Created by 上海柚铺 on 16/4/18.
+//  Copyright © 2016年 上海柚铺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PrimeAndSell/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PrimeAndSell
+//
+//  Created by 上海柚铺 on 16/4/18.
+//  Copyright © 2016年 上海柚铺. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
